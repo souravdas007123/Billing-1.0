12c9be6f1fe68e9d480bb9c39b8f2d0951295713
Sourav Das <souravdas007123@gmail.com>
2025-03-04 22:20:04 +0530

    company name update

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.getElementById("toggle-btn").addEventListener("click", function () {
     let sidebar = document.getElementById("sidebar");
     let toggleIcon = document.getElementById("toggle-icon");
     let companyName = document.getElementById("company-name");
+    let smallName = document.getElementById("name-small");
     sidebar.classList.toggle("collapsed");
     toggleIcon.classList.toggle("rotated");
     companyName.style.display = sidebar.classList.contains("collapsed") ? "none" : "block";
+    smallName.style.display = sidebar.classList.contains("collapsed") ? "block" : "none";
 });
 
 function toggleDarkMode() {
